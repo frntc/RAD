60554ea8870d33d832364543914810e0dc1716c0
frntc <48584231+frntc@users.noreply.github.com>
2025-02-28 14:23:12 +0100

    Add files via upload

--- Source/Firmware/C64Side/make_all.bat
+++ Source/Firmware/C64Side/make_all.bat
@@ -5,3 +5,7 @@ bin2c -o ultimax_init.h ultimax_init
 64tass --nostart ultimax_memcfg.a
 xcopy /Y a.out ultimax_memcfg
 bin2c -o ultimax_memcfg.h ultimax_memcfg
+
+64tass --nostart ultimax_vsf.a
+xcopy /Y a.out ultimax_vsf
+bin2c -o ultimax_vsf.h ultimax_vsf
